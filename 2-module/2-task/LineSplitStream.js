608dc3487865811016f9372f11ed255a13f7175f
Andrey Zhestkov <ajestkov@gmail.com>
2019-04-16 00:13:02 +0300

    module 2

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,25 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.last = '';
+    this.on('error', (e) => {
+      console.log(e);
+    });
   }
-  
+
   _transform(chunk, encoding, callback) {
+    const str = chunk.toString('utf-8');
+    const lines = ((this.last || '') + str).split(os.EOL);
+    this.last = lines.pop();
+    lines.forEach((line) => {
+      this.push(line);
+    });
+    callback();
   }
-  
+
   _flush(callback) {
+    this.push(this.last != null ? this.last : '');
+    callback();
   }
 }
 
